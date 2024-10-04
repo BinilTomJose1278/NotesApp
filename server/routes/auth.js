4b19522dd69f222623c6dfef3c005f1622fb00f4
BINIL TOM JOSE <74092233+BinilTomJose1278@users.noreply.github.com>
2024-10-04 13:22:24 +1000

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,40 +4,43 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/User");
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
-    },
-    async function (accessToken, refreshToken, profile, done) {
-      const newUser = {
-        googleId: profile.id,
-        displayName: profile.displayName,
-        firstName: profile.name.givenName,
-        lastName: profile.name.familyName,
-        profileImage: profile.photos[0].value,
-      };
-      try {
-        let user = await User.findOne({ googleId: profile.id });
-        if (user) {
-          done(null, user);
-        } else {
-          user = await User.create(newUser);
-          done(null, user);
+// Check if environment variables are set
+if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET && process.env.GOOGLE_CALLBACK_URL) {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      },
+      async function (accessToken, refreshToken, profile, done) {
+        const newUser = {
+          googleId: profile.id,
+          displayName: profile.displayName,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          profileImage: profile.photos[0].value,
+        };
+        try {
+          let user = await User.findOne({ googleId: profile.id });
+          if (user) {
+            done(null, user);
+          } else {
+            user = await User.create(newUser);
+            done(null, user);
+          }
+        } catch (error) {
+          console.log(error);
         }
-      } catch (error) {
-        console.log(error);
       }
-    }
-  )
-);
+    )
+  );
+} else {
+  console.warn('Google OAuth credentials are not set. Skipping Google Strategy initialization.');
+}
 
-router.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["email", "profile"] })
-);
+// Authentication routes
+router.get("/auth/google", passport.authenticate("google", { scope: ["email", "profile"] }));
 
 router.get(
   "/google/callback",
@@ -55,15 +58,18 @@ router.get("/logout", (req, res) => {
   req.session.destroy((error) => {
     if (error) {
       console.log(error);
-      res.send("Error loggin out");
+      res.send("Error logging out");
     } else {
       res.redirect("/");
     }
   });
 });
+
+// Serialization and Deserialization
 passport.serializeUser(function (user, done) {
   done(null, user.id);
 });
+
 passport.deserializeUser(async (id, done) => {
   try {
     const user = await User.findById(id);
